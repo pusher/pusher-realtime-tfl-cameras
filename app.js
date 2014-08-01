760b1d7ed0709f3051ccd9f2f3dfa8ffed3a432f
Robin Hawkes <rob@rawkes.com>
2014-08-01 17:10:32 +0100

    Added map

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ try {
   }
 }
 
+var express = require("express");
+var bodyParser = require("body-parser");
+var errorHandler = require("errorhandler");
+
+var app = express();
+var root = __dirname + "/demo";
+
+
 // --------------------------------------------------------------------
 // SET UP PUSHER
 // --------------------------------------------------------------------
@@ -29,6 +37,41 @@ var pusher = new Pusher({
   key: config.key,
   secret: config.secret
 });
+
+// --------------------------------------------------------------------
+// SET UP EXPRESS
+// --------------------------------------------------------------------
+
+// Parse application/json and application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+// Simple logger
+app.use(function(req, res, next){
+  console.log("%s %s", req.method, req.url);
+  console.log(req.body);
+  next();
+});
+
+// Error handler
+app.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}));
+
+// Serve static files from directory
+app.use(express.static(root));
+
+// Get all cameras
+app.get("/cameras", function(req, res) {
+  res.json(cameras);
+});
+
+// Open server on specified port
+console.log("Starting Express server");
+app.listen(process.env.PORT || 5001);
 
 // --------------------------------------------------------------------
 // TfL
---
+++ demo/pusher-tfl-cameras-map.js
@@ -0,0 +1,64 @@
+// Enable pusher logging - don't include this in production
+// Pusher.log = function(message) {
+//   if (window.console && window.console.log) {
+//     window.console.log(message);
+//   }
+// };
+
+var tflURL = "http://tfl.gov.uk/tfl/livetravelnews/trafficcams/cctv/";
+
+var map = L.map("map").setView([51.505, -0.09], 13);
+var cameraMarkers = {};
+
+L.tileLayer("https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png", {
+  maxZoom: 18,
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  id: 'examples.map-i86knfo3'
+}).addTo(map);
+
+$.getJSON("/cameras", function(cameras) {
+  if (_.size(cameras) > 0) {
+    addCamerasToMap(cameras);
+  }
+});
+
+var addCamerasToMap = function(cameras) {
+  var markers = new L.MarkerClusterGroup();
+  var markerLayer = [];
+
+  _.each(cameras, function(camera, index) {
+    var id = camera.file.replace(".jpg", "");
+    var marker = new L.marker([camera.lat, camera.lng]).bindPopup(cameraMarkerContent(id), {maxWidth: 352});
+    
+    cameraMarkers[id] = marker;
+    markerLayer.push(marker);
+  });
+
+  markers.addLayers(markerLayer);
+  markers.addTo(map);
+};
+
+var cameraMarkerContent = function(id) {
+  return '<img src="' + (tflURL + id + '.jpg?' + Date.now()) + '" class="camera-feed" height="288" width="352">';
+}
+
+// var cameras = document.querySelector(".cameras");
+
+var pusher = new Pusher("f1b8177ecbc7a66de0c7");
+var channel = pusher.subscribe("cameras");
+channel.bind("cameras-update", function(data) {
+  var cameraURLs = data.split("|");
+  var newCameras = 0;
+
+  var marker;
+  _.each(cameraURLs, function(url, index) {
+    marker = cameraMarkers[url];
+    marker.setPopupContent(cameraMarkerContent(url));
+  });
+
+  console.log(cameraURLs.length + " updated cameras");
+  console.log(newCameras + " new cameras");
+  console.log(document.getElementsByClassName("camera-feed").length - newCameras + " existing cameras");
+});
