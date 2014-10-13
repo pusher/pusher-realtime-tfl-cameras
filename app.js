d2bb06660ac3fa2ed2f8de3dba1f453d489d8ca4
Robin Hawkes <rob@rawkes.com>
2014-10-13 12:44:53 +0100

    Removed demo

--- app.js
+++ app.js
@@ -22,13 +22,6 @@ try {
   }
 }
 
-var express = require("express");
-var bodyParser = require("body-parser");
-var errorHandler = require("errorhandler");
-
-var app = express();
-var root = __dirname + "/demo";
-
 
 // --------------------------------------------------------------------
 // SET UP PUSHER
@@ -40,40 +33,6 @@ var pusher = new Pusher({
   secret: config.secret
 });
 
-// --------------------------------------------------------------------
-// SET UP EXPRESS
-// --------------------------------------------------------------------
-
-// Parse application/json and application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
-
-// Simple logger
-app.use(function(req, res, next){
-  console.log("%s %s", req.method, req.url);
-  console.log(req.body);
-  next();
-});
-
-// Error handler
-app.use(errorHandler({
-  dumpExceptions: true,
-  showStack: true
-}));
-
-// Serve static files from directory
-app.use(express.static(root));
-
-// Get all cameras
-app.get("/cameras", function(req, res) {
-  res.json(cameras);
-});
-
-// Open server on specified port
-if (!silent) console.log("Starting Express server");
-app.listen(process.env.PORT || 5001);
 
 // --------------------------------------------------------------------
 // TfL
